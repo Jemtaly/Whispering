85405923fda4b50e9070c6bbc90fdcf633a72188
Jemtaly <Jemtaly@outlook.com>
2024-04-05 23:28:08 +0800

    Improve readability

--- whispering.py
+++ whispering.py
@@ -13,30 +13,35 @@ def translate(text, source, target):
     ans = requests.get(url).json()[0] or []
     return [(t, s) for t, s, *infos in ans]
 class Text(tk.Text):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, master, res_queue):
+        super().__init__(master)
         self.tag_config('done', foreground = 'black')
         self.tag_config('curr', foreground = 'blue')
         self.insert(tk.END, '  ', 'done')
         self.record = self.index('end-1c')
         self.config(state = tk.DISABLED)
-    def update(self, res):
-        self.config(state = tk.NORMAL)
-        if res is not True:
-            done_str, curr_str = res
-            self.delete(self.record, tk.END)
-            if done_str is not None:
+        self.res_queue = res_queue
+        self.after(1, self.update)
+    def update(self):
+        while not self.res_queue.empty():
+            res = self.res_queue.get()
+            self.config(state = tk.NORMAL)
+            if res is not True:
+                done_str, curr_str = res
+                self.delete(self.record, tk.END)
+                if done_str is not None:
+                    self.insert(tk.END, done_str, 'done')
+                    self.record = self.index('end-1c')
+                self.insert(tk.END, curr_str, 'curr')
+            elif self.index('end-1c').split('.')[1] != '2':
+                done_str = self.get(self.record, tk.END).rstrip('\n') + '\n'
+                self.delete(self.record, tk.END)
                 self.insert(tk.END, done_str, 'done')
+                self.insert(tk.END, '  ', 'done')
                 self.record = self.index('end-1c')
-            self.insert(tk.END, curr_str, 'curr')
-        elif self.index('end-1c').split('.')[1] != '2':
-            done_str = self.get(self.record, tk.END).rstrip('\n') + '\n'
-            self.delete(self.record, tk.END)
-            self.insert(tk.END, done_str, 'done')
-            self.insert(tk.END, '  ', 'done')
-            self.record = self.index('end-1c')
-        self.see(tk.END)
-        self.config(state = tk.DISABLED)
+            self.see(tk.END)
+            self.config(state = tk.DISABLED)
+        self.after(1, self.update)
 class Queue:
     def __init__(self):
         self.queue = collections.deque()
@@ -54,11 +59,6 @@ class Queue:
                 self.cond.wait()
             return self.queue.popleft()
 def transcribe(size, device, latency, patience, eager, amnesia, prompt, attension, source, target):
-    root = tk.Tk()
-    ts_text = Text(root)
-    ts_text.pack(expand = True, fill = 'both', side = 'left')
-    tl_text = Text(root)
-    tl_text.pack(expand = True, fill = 'both', side = 'right')
     model = WhisperModel(size)
     recognizer = sr.Recognizer()
     mic = sr.Microphone(device)
@@ -83,65 +83,61 @@ def transcribe(size, device, latency, patience, eager, amnesia, prompt, attensio
         while frame := frame_queue.get():
             if frame is True:
                 window = bytearray()
-                prev_tsc = prompt
+                prev_src = prompt
                 ts2tl_queue.put(True)
                 tsres_queue.put(True)
                 continue
             window.extend(frame)
             audio = sr.AudioData(window, mic.SAMPLE_RATE, mic.SAMPLE_WIDTH)
             with io.BytesIO(audio.get_wav_data()) as audio_file:
-                segments, info = model.transcribe(audio_file, language = source, initial_prompt = prev_tsc)
+                segments, info = model.transcribe(audio_file, language = source, initial_prompt = prev_src)
             segments = list(segments)
             if len(segments) > attension:
-                done_tsc = ''.join(segment.text for segment in segments[:-attension])
-                curr_tsc = ''.join(segment.text for segment in segments[-attension:])
+                done_src = ''.join(segment.text for segment in segments[:-attension])
+                curr_src = ''.join(segment.text for segment in segments[-attension:])
                 window = window[int(segments[-attension].start * mic.SAMPLE_WIDTH * mic.SAMPLE_RATE):]
-                prev_tsc = segments[-attension - 1].text if amnesia else prev_tsc + done_tsc
+                prev_src = segments[-attension - 1].text if amnesia else prev_src + done_src
             else:
-                done_tsc = None
-                curr_tsc = ''.join(segment.text for segment in segments)
-            ts2tl_queue.put((done_tsc, curr_tsc))
-            tsres_queue.put((done_tsc, curr_tsc))
+                done_src = None
+                curr_src = ''.join(segment.text for segment in segments)
+            ts2tl_queue.put((done_src, curr_src))
+            tsres_queue.put((done_src, curr_src))
         ts2tl_queue.put(False)
     def tl_fun():
         while ts2tl := ts2tl_queue.get():
             if ts2tl is True:
-                pref_tsc = ''
+                pref_src = ''
                 tlres_queue.put(True)
                 continue
-            done_tsc, curr_tsc = ts2tl
-            if done_tsc is not None:
-                done_tsc = pref_tsc + done_tsc
-                full_tsc = done_tsc + curr_tsc
-                done_res = translate(done_tsc, source or 'auto', target)
-                full_res = translate(full_tsc, source or 'auto', target)
+            done_src, curr_src = ts2tl
+            if done_src is not None:
+                done_src = pref_src + done_src
+                full_src = done_src + curr_src
+                done_res = translate(done_src, source or 'auto', target)
+                full_res = translate(full_src, source or 'auto', target)
                 done_len = len(done_res) - 1
                 if done_res[done_len][1] == full_res[done_len][1]:
                     done_len += 1
-                    pref_tsc = ''
+                    pref_src = ''
                 else:
-                    pref_tsc = done_res[done_len][1]
-                done_tsl = ''.join(t for t, s in full_res[:done_len])
-                curr_tsl = ''.join(t for t, s in full_res[done_len:])
+                    pref_src = done_res[done_len][1]
+                done_tgt = ''.join(t for t, s in full_res[:done_len])
+                curr_tgt = ''.join(t for t, s in full_res[done_len:])
             else:
-                curr_tsc = pref_tsc + curr_tsc
-                curr_res = translate(curr_tsc, source or 'auto', target)
-                done_tsl = None
-                curr_tsl = ''.join(t for t, s in curr_res)
-            tlres_queue.put((done_tsl, curr_tsl))
-    def update(res_queue, text):
-        if not res_queue.empty():
-            res = res_queue.get()
-            text.update(res)
-        root.after(1, update, res_queue, text)
+                curr_src = pref_src + curr_src
+                curr_res = translate(curr_src, source or 'auto', target)
+                done_tgt = None
+                curr_tgt = ''.join(t for t, s in curr_res)
+            tlres_queue.put((done_tgt, curr_tgt))
     listen_thread = threading.Thread(target = listen, daemon = True)
     ts_fun_thread = threading.Thread(target = ts_fun, daemon = True)
     tl_fun_thread = threading.Thread(target = tl_fun, daemon = True)
     listen_thread.start()
     ts_fun_thread.start()
     tl_fun_thread.start()
-    root.after(1, update, tlres_queue, tl_text)
-    root.after(1, update, tsres_queue, ts_text)
+    root = tk.Tk()
+    Text(root, tsres_queue).pack(expand = True, fill = 'both', side = 'left')
+    Text(root, tlres_queue).pack(expand = True, fill = 'both', side = 'right')
     root.mainloop()
     listen_flag[0] = False
     listen_thread.join()
@@ -156,7 +152,7 @@ def main():
     parser.add_argument('--eager', action = 'store_true', help = 'consider all segments in the transcribing window as finished once there is a pause')
     parser.add_argument('--amnesia', action = 'store_true', help = 'only use the last segment as the prompt for the next segment')
     parser.add_argument('--prompt', type = str, default = None, help = 'initial prompt for the first segment')
-    parser.add_argument('--attension', type = int, default = 2, choices = range(1, 4), help = 'maximum number of segments to consider for transcription')
+    parser.add_argument('--attension', type = int, default = 2, choices = range(1, 6), help = 'maximum number of segments to consider for transcription')
     parser.add_argument('--source', type = str, default = None, help = 'source language for translation')
     parser.add_argument('--target', type = str, default = 'en', help = 'target language for translation')
     args = parser.parse_args()
